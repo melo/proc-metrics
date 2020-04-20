2111079744d068877dee4502eef8878c68ca9c8a
Ramin <ramin@firstsource.tech>
2020-04-20 13:04:14 +0430

    - Substitute cpu usage with old value if $dt is equal to 0

--- proc-cpuusage/lib/Proc/CPUUsage.pm
+++ proc-cpuusage/lib/Proc/CPUUsage.pm
@@ -8,15 +8,15 @@ use Time::HiRes qw( gettimeofday tv_interval );
 sub new {
   my $class = shift;
   
-  return bless [ [gettimeofday()], _cpu_time() ], $class;
+  return bless [ [gettimeofday()], _cpu_time(), 0 ], $class;
 }
 
 sub usage {
   my $self = $_[0];
-  my ($t0, $r0) = @$self;
+  my ($t0, $r0, $l0) = @$self;
   return unless defined $r0;
   
-  my ($dt, $dr, $t1, $r1);
+  my ($dt, $dr, $t1, $r1, $l1);
   $t1 = [gettimeofday()];
   $dt = tv_interval($t0, $t1);
   $self->[0] = $t1;
@@ -24,8 +24,11 @@ sub usage {
   $r1 = _cpu_time();
   $dr = $r1 - $r0;
   $self->[1] = $r1;
-  
-  return $dr/$dt;
+
+  $l1 = $dt == 0 ? $l0 : $dr/$dt;
+  $self->[2] = $l1;
+
+  return $l1;
 }
 
 sub _cpu_time {
